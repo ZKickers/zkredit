d4fe82962f6ddba4ded844f3cf082fa8236c3608
usefSaeed <youssef.okab@gmail.com>
2024-03-14 05:49:25 +0200

    ZKT-24 fake api done using rsa ds "

---
+++ Backend/FakeBureauAPI/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const digsig = require('./digitalSig.js')
+const func = require('./functions.js')
+const app = express();
+
+const PATH = 'DummyData/'
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const userData = req.body;
+    const dummyUser = JSON.parse(fs.readFileSync(PATH + 'dummyUser.json', 'utf8'));
+    if (func.compareData(userData, dummyUser)) {
+        const privateKey = fs.readFileSync(PATH + 'privateKey.pem', 'utf8');
+        const dummyReport = JSON.parse(fs.readFileSync(PATH + 'dummyReport.json', 'utf8'));
+        const signature = digsig.genDS_RSA(JSON.stringify(dummyReport), privateKey);
+        dummyReport.signature = signature;
+        res.json(dummyReport);
+    }else{
+        res.status(403).json({ error: 'Data mismatch' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
