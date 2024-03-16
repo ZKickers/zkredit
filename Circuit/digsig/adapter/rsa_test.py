0a97a48b7a251f431c9feb6da588f3b1675d6dc4
usefSaeed <youssef.okab@gmail.com>
2024-03-16 22:01:38 +0200

    rsa despare

--- Circuit/digsig/adapter/rsa_test.py
+++ Circuit/digsig/adapter/rsa_test.py
@@ -9,6 +9,6 @@ print("Hash of message:", msg_hash.hex())
 
 sig_bytes = base64.b64decode(sig_og)
 sig_int = int.from_bytes(sig_bytes, byteorder='big')
-sig_power = pow(sig_int, e_og, n_og) % (1<<256)
+sig_power = pow(sig_int, e_og, n_og)
 sig_power_bytes = bytes_data = sig_power.to_bytes((sig_power.bit_length() + 7) // 8, byteorder='big')
-print("Signature power:", sig_power_bytes.hex())
+print("Signature power:", sig_power_bytes[-32:].hex())
