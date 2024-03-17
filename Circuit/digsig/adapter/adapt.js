9143a500f361afdcd46553b835776dcff3333f70
usefSaeed <youssef.okab@gmail.com>
2024-03-17 23:45:38 +0200

    ZKT-5-test failed sig invalid

--- Circuit/digsig/adapter/adapt.js
+++ Circuit/digsig/adapter/adapt.js
@@ -3,5 +3,6 @@ const resp = require("./msg_handler")
 const fs = require("fs")
 
 const data = [resp.R, resp.S, pk.A, resp.msg]
+console.log("Msg Length:",resp.msg.length)
 const jsonString = JSON.stringify(data, null, 4);
 fs.writeFileSync('digsig_test.json', jsonString, 'utf8');
