400b4f85d0c020214a4d3dec152864aa5e281a28
usefSaeed <youssef.okab@gmail.com>
2024-03-17 02:18:20 +0200

    done secp256k1 param

--- Circuit/digsig/adapter/generate.py
+++ Circuit/digsig/adapter/generate.py
@@ -1,9 +1,20 @@
 from msg_handler import msg,sig_bytes
 from pk_handler import public_key,A
+from ecdsa import numbertheory
 import json
 import ecdsa
 R = [0,0]
 R[0], S = ecdsa.util.sigdecode_der(sig_bytes, public_key.pubkey.order)
+p = 2**256 - 2**32 - 977
+y_squared = (pow(R[0], 3, p) + 7) % p
+try:
+    R[1] = numbertheory.square_root_mod_prime(y_squared, p)
+except ValueError:
+    print("No y coordinate exists for the given x on the secp256k1 curve.")
+
+curve = ecdsa.curves.SECP256k1.curve
+
+test = ecdsa.ellipticcurve.Point(curve, R[0],R[1], True)
 
 print(f"R: {R}")
 print(f"S: {S}")
