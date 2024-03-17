575afb64626c2a6c12796bda6bda774cfb88eaa1
usefSaeed <youssef.okab@gmail.com>
2024-03-17 22:35:32 +0200

    ZKT-5-input modified

--- Circuit/digsig/adapter/adapt.js
+++ Circuit/digsig/adapter/adapt.js
@@ -2,6 +2,6 @@ const pk = require("./pk_handler")
 const resp = require("./msg_handler")
 const fs = require("fs")
 
-const data = [resp.R, resp.S, resp.msg, pk.A]
+const data = [resp.R, resp.S, pk.A, resp.msg]
 const jsonString = JSON.stringify(data, null, 4);
 fs.writeFileSync('digsig_test.json', jsonString, 'utf8');
