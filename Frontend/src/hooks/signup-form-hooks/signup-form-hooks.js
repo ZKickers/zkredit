2c21df6cd883f99b665f7e26deb6239082e825f6
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-03-18 20:53:01 +0200

    Update signup-form-hooks.js

--- Frontend/src/hooks/signup-form-hooks/signup-form-hooks.js
+++ Frontend/src/hooks/signup-form-hooks/signup-form-hooks.js
@@ -1,11 +1,9 @@
 import useUsernameValidation from "hooks/signup-form-hooks/use-username-validation";
 import useEmailValidation from "hooks/signup-form-hooks/use-email-validation";
 import usePasswordValidation from "hooks/signup-form-hooks/use-password-validation";
-import useOutsideClick from "hooks/signup-form-hooks/use-modal-close";
 
 export {
   useUsernameValidation,
   useEmailValidation,
   usePasswordValidation,
-  useOutsideClick,
 };
