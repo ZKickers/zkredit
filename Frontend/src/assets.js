7f38db4b61245d2183b0b4be091aa5a9def311e7
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-03-18 19:46:52 +0200

    Added assets

--- Frontend/src/assets.js
+++ Frontend/src/assets.js
@@ -1,5 +1,17 @@
 import checkMark from "assets/Check.png";
 import computer from "assets/2007.i039 1.png";
-import signinIcon from "assets/sign-in-icon.png"
+import signinIcon from "assets/sign-in-icon.png";
+import keyIcon from "assets/key.png";
+import checkKeyIcon from "assets/check-key.png";
+import profileIcon from "assets/profile.png";
+import emailIcon from "assets/email.png";
 
-export { checkMark, computer, signinIcon };
+export {
+  checkMark,
+  computer,
+  signinIcon,
+  keyIcon,
+  checkKeyIcon,
+  profileIcon,
+  emailIcon,
+};
