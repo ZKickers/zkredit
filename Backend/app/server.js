413478d0a4f3d5e1937300aa56e5925076ac80de
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2024-03-19 22:57:19 +0200

    modifications

--- Backend/app/server.js
+++ Backend/app/server.js
@@ -18,7 +18,7 @@ app.use('/auth', authRoutes);
 app.use('/gettransaction', gettransaction);
 app.use('/deletetransaction', deletedTransaction);
 app.use('/ClientRequest', ClientRequest);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
