37ca6656b34e8da66f828de298139f56e86c0236
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2024-03-19 04:27:24 +0200

    edit in delete transcation

--- Backend/app/routes/deletetransactions.js
+++ Backend/app/routes/deletetransactions.js
@@ -3,17 +3,16 @@ const router = express.Router();
 const Transaction = require('../models/Transaction');
 const verifyToken = require('../Services/authMiddleware');
 // Route to delete a transaction by ID
-router.delete('/transactions/:transactionId', verifyToken, async (req, res) => {
+router.delete('/transactions/client/:clientId', verifyToken, async (req, res) => {
     try {
-        const deletedTransaction = await Transaction.findByIdAndDelete(req.params.transactionId);
-        if (!deletedTransaction) {
-            return res.status(404).send('Transaction not found');
+        const deletedTransactions = await Transaction.deleteMany({ clientAccountId: req.params.clientId });
+        if (deletedTransactions.deletedCount === 0) {
+            return res.status(404).send('No transactions found for the client');
         }
-        res.status(200).json(deletedTransaction);
+        res.status(200).json({ message: 'Transactions deleted successfully' });
     } catch (error) {
-        console.error('Error deleting transaction:', error);
+        console.error('Error deleting transactions:', error);
         res.status(500).send('Internal server error');
     }
 });
-
 module.exports = router;
