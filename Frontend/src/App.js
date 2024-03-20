3e4ae4028a6faaffd11c714e35ccb0e581766693
usefSaeed <youssef.okab@gmail.com>
2024-03-21 01:14:35 +0200

    ZKT-38 changed auth context loc

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,7 @@
 import LandingPage from "pages/landing-page/LandingPage";
-import { AuthContextProvider } from "store/auth-context";
 
 export default function App() {
   return (
-    <AuthContextProvider>
       <LandingPage />
-    </AuthContextProvider>
   );
 }
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import { AuthContextProvider } from "store/auth-context";
 import App from "App";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
-root.render(<App />);
+root.render(
+    <AuthContextProvider>
+        <App />
+    </AuthContextProvider>
+);
