fbd2f664fb0d7b8907618fe4292f4a96bae85d64
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-03-20 17:21:33 +0200

    Update index.js
    Supported routing

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "App";
+import { NavigationProvider } from "contexts/NavigationContext";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
