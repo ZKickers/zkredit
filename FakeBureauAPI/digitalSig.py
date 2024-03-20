f3045080b60a73484590ee22178b163164cb2726
usefSaeed <youssef.okab@gmail.com>
2024-03-20 05:19:52 +0200

    ZKT-35 conc done

--- FakeBureauAPI/digitalSig.py
+++ FakeBureauAPI/digitalSig.py
@@ -1,5 +1,5 @@
 from zokrates_pycrypto.eddsa import PrivateKey, FQ, Point
-from functions import pem_to_hex,PRIVATE,sha256Padded
+from functions import pem_to_hex,PRIVATE,sha256Padded,conc_json
 
 def decompress_point(p):
     p = Point.decompress(bytes.fromhex(p))
@@ -9,6 +9,7 @@ def sign_bjj(msg,sk):
     sk_hex = pem_to_hex(sk,PRIVATE)
     sk_hex = int(sk_hex, 16)
     sk = PrivateKey(FQ(sk_hex))
+    msg = conc_json(msg)
     msg = sha256Padded(msg)
     print("Message Hex:",msg)
     (r_int, s_int) = sk.sign(bytes.fromhex(msg))
