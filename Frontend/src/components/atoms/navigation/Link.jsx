8243e9c67c3ef8f45e015a1587af56687549cc06
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-03-20 17:20:10 +0200

    Reformatted and updated Link.jsx

--- Frontend/src/components/atoms/navigation/Link.jsx
+++ Frontend/src/components/atoms/navigation/Link.jsx
@@ -1,23 +1,29 @@
 import classNames from "classnames";
 import useNavigation from "hooks/use-navigation";
 
-
 export default function Link({ to, children, className, activeClassName }) {
-    const { navigate, path } = useNavigation()
+  const { navigate, path } = useNavigation();
 
-    const classes = classNames(
-        className,
-        path === to && activeClassName,
-    )
+  const classes = classNames(className, path === to && activeClassName);
 
-    const handleClick = (event) => {
-        if (event.metaKey || event.ctrlKey) {
-            return;
-        }
-
-        event.preventDefault();
-        navigate(to);
+  const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
     }
 
-    return <a className={classes} href={to} onClick={handleClick}> {children} </a>
+    event.preventDefault();
+    navigate(to);
+  };
+
+  return (
+    <a
+      className={classes}
+      href={to}
+      onClick={handleClick}
+      style={{ textDecoration: "none", cursor: "pointer" }}
+    >
+      {" "}
+      {children}{" "}
+    </a>
+  );
 }
