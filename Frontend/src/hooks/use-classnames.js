e8c004caed0a97bf0e4a0ca89f2b0dd30da5c8ff
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-03-21 04:07:19 +0200

    Create use-classnames.js
    Reusable class names for different templates

---
+++ Frontend/src/hooks/use-classnames.js
@@ -0,0 +1,52 @@
+import classNames from "classnames";
+import "./fill-height.css";
+
+const pageClasses = classNames(
+  "row",
+  "fill-height",
+  "container-fluid",
+  "mx-auto",
+  "d-flex",
+  "flex-md-row",
+  "flex-sm-column",
+  "justify-content-md-between",
+  "justify-content-sm-start",
+  "align-items-md-start",
+  "align-items-sm-center",
+  "p-4"
+);
+
+const leftClasses = classNames(
+  "col-md-7",
+  "col-sm-12",
+  "d-flex",
+  "flex-column",
+  "align-items-sm-center",
+  "align-items-md-start",
+  "h-100"
+);
+
+const txBox = classNames(
+  "d-flex",
+  "flex-column",
+  "align-items-center",
+  "justify-content-around",
+  "p-4",
+  "w-100",
+  "h-100",
+  "rounded border border-black border-2",
+  "overflow-auto"
+);
+
+const rightClasses = classNames(
+  "col-md-5",
+  "col-sm-12",
+  "d-flex",
+  "flex-column",
+  "justify-content-center",
+  "align-items-center",
+  "h-100",
+  "p-5",
+);
+
+export { pageClasses, leftClasses, txBox, rightClasses };
