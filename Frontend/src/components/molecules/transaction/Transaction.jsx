1f8b5eec083479480c4c17a0245c33966e603c4e
Zeyad Zidan <iamzeyadzidan@gmail.com>
2024-03-22 07:38:44 +0200

    ZKT-45-Dynamic Txs Handling
    Updated all pages and styling to handle dynamic transactions fetching and adding using the newly introduced redux txs api.

--- Frontend/src/components/molecules/transaction/Transaction.jsx
+++ Frontend/src/components/molecules/transaction/Transaction.jsx
@@ -1,35 +1,78 @@
 import classNames from "classnames";
 import "./Transaction.css";
 import { LockIcon, LockOpenIcon, CloseIcon } from "assets";
+import { useFetchCreditorUsernameQuery } from "store/apis/txApi";
+import { datePrettier } from "./datePrettier";
+import TxCard from "../transaction-card/TxCard";
 
-export default function Transaction(props) {
-  const {
-    date,
-    CRID,
-    CLID,
-    verified,
-    declined,
-    pendingThreshold,
-    pendingVerification,
-  } = props;
+export default function Transaction({
+  token,
+  txId,
+  clientFullName,
+  isClient,
+  creditorId,
+  updateDate,
+  status,
+  isButton,
+  renderCard,
+}) {
+  let verified, declined, pendingThreshold, pendingValidation;
+
+  switch (status) {
+    case "Success":
+      verified = true;
+      break;
+    case "Fail":
+      declined = true;
+      break;
+    case "Pending_Threshold":
+      pendingThreshold = true;
+      break;
+    case "Pending_Validation":
+      pendingValidation = true;
+      break;
+  }
+
+  const { formattedDate, formattedTime } = datePrettier(updateDate);
+
+  let creditorUsername;
+
+  const { data, error, isFetching } = useFetchCreditorUsernameQuery({
+    token,
+    creditorId,
+    txId,
+  });
+
+  if (data !== undefined) creditorUsername = data;
+
+  const handleShowCard = () =>
+    renderCard({
+      date: `${formattedDate} ${formattedTime}`,
+      creditorUsername,
+      clientFullName,
+      verified,
+      declined,
+      pendingThreshold,
+      pendingValidation,
+    });
 
   const base = "tx row container w-100 p-md-2 p-sm-3 my-md-1 my-sm-3";
   const classes = classNames(base, {
     "tx-verified": verified,
     "tx-failed": declined,
-    "tx-pending": pendingThreshold || pendingVerification,
+    "tx-pending": pendingThreshold || pendingValidation,
   });
 
-  const status = classNames({
+  const statusText = classNames({
     Verified: verified,
     Declined: declined,
-    Pending: pendingThreshold || pendingVerification,
+    Pending: pendingThreshold || pendingValidation,
   });
 
   const color = classNames({
     "#009A2B": verified,
     "#F62525": declined,
-    "#FFB800": pendingThreshold || pendingVerification,
+    "#FFB800": pendingThreshold || pendingValidation,
   });
 
   const iconStyle = {
@@ -41,8 +84,10 @@ export default function Transaction(props) {
   const renderIcon = () => {
     if (verified)
       return <LockIcon className="rounded-circle p-2" sx={iconStyle} />;
-    if (declined) return <CloseIcon className="rounded-circle p-2" sx={iconStyle} />;
-    if (pendingThreshold || pendingVerification) return <LockOpenIcon className="rounded-circle p-2" sx={iconStyle} />;
+    if (declined)
+      return <CloseIcon className="rounded-circle p-2" sx={iconStyle} />;
+    if (pendingThreshold || pendingValidation)
+      return <LockOpenIcon className="rounded-circle p-2" sx={iconStyle} />;
   };
 
   const contentClasses = classNames(
@@ -63,12 +108,15 @@ export default function Transaction(props) {
     "justify-content-sm-center"
   );
 
-  return (
-    <div className={classes}>
+  const renderContent = () => (
+    <>
       <div className={contentClasses}>
-        <h1 style={{ fontSize: "24px", fontWeight: "bold" }}>{date}</h1>
-        <h2 style={{ fontSize: "20px", fontWeight: "400" }}>{CRID}</h2>
-        <h2 style={{ fontSize: "20px", fontWeight: "400" }}>{CLID}</h2>
+        <h1 style={{ fontSize: "24px", fontWeight: "bold" }}>
+          {formattedDate + " " + formattedTime}
+        </h1>
+        <h2 style={{ fontSize: "20px", fontWeight: "400" }}>
+          {isClient ? creditorUsername : clientFullName}
+        </h2>
         <h3
           style={{
             fontSize: "16px",
@@ -76,10 +124,16 @@ export default function Transaction(props) {
             color: color,
           }}
         >
-          {status}
+          {statusText}
         </h3>
       </div>
       <div className={imgClasses}>{renderIcon()}</div>
-    </div>
+    </>
+  );
+
+  return isButton ? (
+    <button className={classes} onClick={handleShowCard}>{renderContent()}</button>
+  ) : (
+    <div className={classes}>{renderContent()}</div>
   );
 }
