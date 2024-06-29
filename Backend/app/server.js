9652b7ca7d125933f174aed245d78f040c9b3cf7
Omar khairat <omarkhairat133@gmail.com>
2024-06-29 15:59:46 +0300

    fixing backend csrf

--- Backend/app/server.js
+++ Backend/app/server.js
@@ -3,28 +3,28 @@ const https = require('https');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
-const getTX = require('./routes/getTX')
-const cors = require('cors')
+const getTX = require('./routes/getTX');
+const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const csurf = require('csurf');
 const fs = require('fs');
 const app = express();
-const deleteTX = require('./routes/deleteTXRoute')
-const csrfRoute = require('./routes/csrf')
+const deleteTX = require('./routes/deleteTXRoute');
+const csrfRoute = require('./routes/csrf');
 const ClientRequest = require('./routes/ClientRequest');
-const thresholdRoute = require('./routes/thresholdRoute')
-const verificationRoutes = require('./routes/verificationRoutes')
-const verificationKeyRoute = require('./routes/verificationKeyRoute')
-const getProofRoute = require('./routes/getProofRoute')
+const thresholdRoute = require('./routes/thresholdRoute');
+const verificationRoutes = require('./routes/verificationRoutes');
+const verificationKeyRoute = require('./routes/verificationKeyRoute');
+const getProofRoute = require('./routes/getProofRoute');
 const verifyToken = require('./middlewares/authMiddleware');
-const {ipGeneralLimiter, ipProofLimiter, generalAccountLimiter, proofAccountLimiter} = require('./middlewares/rateLimiter');
+const { ipGeneralLimiter, ipProofLimiter, generalAccountLimiter, proofAccountLimiter } = require('./middlewares/rateLimiter');
 const { BACKEND_PORT, FRONTEND_URL, MONGODB_URI } = require('../config');
 
 app.use(cors({
-  origin: FRONTEND_URL
+  origin: FRONTEND_URL,
+  credentials: true // Ensure cookies are sent with requests
 }));
-
 
 app.use(helmet());
 app.use(cookieParser());
@@ -32,6 +32,7 @@ app.use(bodyParser.json({ limit: '1mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
 app.use(ipGeneralLimiter);
 app.use('/ClientRequest/generate-proof', ipProofLimiter);
+
 const csrfProtection = csurf({
   cookie: {
     httpOnly: true,
@@ -39,23 +40,25 @@ const csrfProtection = csurf({
     sameSite: 'strict'
   }
 });
+
 app.use(csrfProtection);
 app.use((req, res, next) => {
-  res.locals.csrfToken = req.csrfToken();
-  console.log('Logging with:' );
-  console.log(req.csrfToken())
+  const csrfToken = req.csrfToken();
+  console.log('Generated CSRF Token: ', req.csrfToken());
+  res.cookie('XSRF-TOKEN', csrfToken); // Set CSRF token in cookie
+  res.locals.csrfToken = csrfToken;
   next();
 });
+
 app.use('/csrf-token', csrfRoute);
 app.use('/auth', authRoutes);
 
 app.use(verifyToken, generalAccountLimiter);
 app.use('/ClientRequest/generate-proof', verifyToken, proofAccountLimiter);
 
-
 mongoose.connect(MONGODB_URI)
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/getTX', getTX);
 app.use('/deleteTX', deleteTX);
@@ -65,32 +68,29 @@ app.use('/verifyTx', verificationRoutes);
 app.use('/verification-key', verificationKeyRoute);
 app.use('/getProof', getProofRoute);
 
-
 app.use((err, req, res, next) => {
   if (err.code === 'EBADCSRFTOKEN') {
+    // console.log("FFF   "+ );
+    console.error('CSRF token validation failed');
     res.status(403).json({ message: 'CSRF token validation failed' });
   } else {
     next(err);
   }
 });
+
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 413 && 'body' in err) {
-      res.status(413).json({ error: 'Payload too large' });
+    res.status(413).json({ error: 'Payload too large' });
   } else {
-      next();
+    next();
   }
 });
-
-// app.listen(BACKEND_PORT, () => {
-//   console.log(`Backend Server is running on port ${BACKEND_PORT}`);
-// });
 
 const sslOptions = {
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert')
 };
 
-// Create HTTPS server
 https.createServer(sslOptions, app).listen(BACKEND_PORT, () => {
   console.log(`Backend Server is running on port ${BACKEND_PORT}`);
 });
